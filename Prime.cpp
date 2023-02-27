ebe1a8a0d3d8904520faac47601fbf63f01eeddd
ishanundre2004 <112792730+ishanundre2004@users.noreply.github.com>
2023-02-27 23:31:46 +0530

    Add files via upload

---
+++ Prime.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int num;
+	cout<<"Enter the num : ";
+	cin>>num;
+	for(int i=1; i<num; i++)
+	{
+		int div=num/i;
+		cout<<div<<endl;
+	}
+	
+}
