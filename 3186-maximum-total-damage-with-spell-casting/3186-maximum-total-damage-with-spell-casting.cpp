b6c213cd143bd1e9a5a5eedc3a858e90ecc72a11
Ishan Undre <112792730+ishanundre2004@users.noreply.github.com>
2025-10-11 09:38:27 +0530

    Time: 391 ms (38.13%), Space: 216.4 MB (36.57%) - LeetHub

--- 3186-maximum-total-damage-with-spell-casting/3186-maximum-total-damage-with-spell-casting.cpp
+++ 3186-maximum-total-damage-with-spell-casting/3186-maximum-total-damage-with-spell-casting.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
-    long long solve(int i, vector<int>& nums, vector<int>& freq, vector<long long>& dp) {
+    long long solve(int i, vector<int>& nums, vector<int>& freq,
+                    vector<long long>& dp) {
         if (i == 0)
             return 1LL * nums[0] * freq[0];
         if (i < 0)
             return 0;
 
-        if(dp[i] != -1) return dp[i];
+        if (dp[i] != -1)
+            return dp[i];
 
         auto it = lower_bound(nums.begin(), nums.end(), nums[i] - 2);
         int ind = -1;
@@ -16,7 +18,7 @@ public:
         }
 
         long long notake = solve(i - 1, nums, freq, dp);
-        
+
         long long take = nums[i] * freq[i] + solve(ind, nums, freq, dp);
 
         return dp[i] = max(notake, take);
@@ -34,6 +36,24 @@ public:
         }
         int n = nums.size();
         vector<long long> dp(n, -1);
-        return solve(n - 1, nums, freq, dp);
+        dp[0] = 1LL * nums[0] * freq[0];
+
+        for (int i = 1; i < n; i++) {
+            auto it = lower_bound(nums.begin(), nums.end(), nums[i] - 2);
+            int ind = -1;
+            if (it != nums.begin()) {
+                --it;
+                ind = it - nums.begin();
+            }
+            long long notake = dp[i-1];
+            long long take = 1LL * nums[i] * freq[i];
+
+            if(ind >= 0){
+                take += dp[ind];
+            }
+
+            dp[i] = max(take, notake);
+        }
+        return dp[n-1];
     }
 };
