574273cc4f18f0cb9a0cd9afdca7b24345629013
Ishan Undre <112792730+ishanundre2004@users.noreply.github.com>
2025-10-11 09:32:53 +0530

    Time: 415 ms (32.41%), Space: 224.7 MB (26.52%) - LeetHub

---
+++ 3186-maximum-total-damage-with-spell-casting/3186-maximum-total-damage-with-spell-casting.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    long long solve(int i, vector<int>& nums, vector<int>& freq, vector<long long>& dp) {
+        if (i == 0)
+            return 1LL * nums[0] * freq[0];
+        if (i < 0)
+            return 0;
+
+        if(dp[i] != -1) return dp[i];
+
+        auto it = lower_bound(nums.begin(), nums.end(), nums[i] - 2);
+        int ind = -1;
+        if (it != nums.begin()) {
+            --it;
+            ind = it - nums.begin();
+        }
+
+        long long notake = solve(i - 1, nums, freq, dp);
+        
+        long long take = nums[i] * freq[i] + solve(ind, nums, freq, dp);
+
+        return dp[i] = max(notake, take);
+    }
+
+    long long maximumTotalDamage(vector<int>& power) {
+        map<int, int> mpp;
+        for (auto& it : power)
+            mpp[it]++;
+        vector<int> nums;
+        vector<int> freq;
+        for (auto& it : mpp) {
+            nums.push_back(it.first);
+            freq.push_back(it.second);
+        }
+        int n = nums.size();
+        vector<long long> dp(n, -1);
+        return solve(n - 1, nums, freq, dp);
+    }
+};
