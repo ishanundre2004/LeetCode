6efa0bc5592d3424dafae8f857d7491704e8bba1
Ishan Undre <112792730+ishanundre2004@users.noreply.github.com>
2025-10-14 09:28:25 +0530

    Time: 27 ms (5.31%), Space: 40.5 MB (53.98%) - LeetHub

--- 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.cpp
+++ 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
-    bool isIncreasing(int i, int k, vector<int>& nums){
-        for(int j = i; j < i+k-1; j++){
-            if(nums[j] >= nums[j+1]) return false;
+    bool isIncreasing(int i, int k, vector<int>& nums) {
+        for (int j = i; j < i + k - 1; j++) {
+            if (nums[j] >= nums[j + 1]) return false;
         }
         return true;
     }
+
     bool hasIncreasingSubarrays(vector<int>& nums, int k) {
         int n = nums.size();
-        for(int i = 0; i <= n-(2*k); i++){
-            if(isIncreasing(i, k, nums) && isIncreasing(i+k, k ,nums)) return true;
+        for (int i = 0; i <= n - 2 * k; i++) {
+            if (isIncreasing(i, k, nums) && isIncreasing(i + k, k, nums))
+                return true;
         }
         return false;
+    }
+
+    int maxIncreasingSubarrays(vector<int>& nums) {
+        int n = nums.size();
+        int l = 1, h = n / 2;
+        int ans = 0;
+        while (l <= h) {
+            int m = l + (h - l) / 2;
+            if (hasIncreasingSubarrays(nums, m)) {
+                ans = m;
+                l = m + 1;
+            } else {
+                h = m - 1;
+            }
+        }
+        return ans;
     }
 };
