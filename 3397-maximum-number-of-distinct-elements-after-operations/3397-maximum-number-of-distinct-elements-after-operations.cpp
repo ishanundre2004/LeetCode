b2bea652e2e761fc98b324a92ab90f7dba4a6772
Ishan Undre <112792730+ishanundre2004@users.noreply.github.com>
2025-10-20 08:52:43 +0530

    Time: 120 ms (55.52%), Space: 97.6 MB (87.36%) - LeetHub

--- 3397-maximum-number-of-distinct-elements-after-operations/3397-maximum-number-of-distinct-elements-after-operations.cpp
+++ 3397-maximum-number-of-distinct-elements-after-operations/3397-maximum-number-of-distinct-elements-after-operations.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     int maxDistinctElements(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
-        int n = nums.size();
-        int h = nums[n-1]+k;
-        int l = nums[0]-k;
-        return min(h-l+1, n);
+        int last = nums[0] - k - 1, uniqs = 0;
+        for(int i : nums){
+            if(i-k > last && ++uniqs) last = i-k;
+            else if(last < i+k && ++uniqs) last++;
+        }
+        return uniqs;
     }
 };
